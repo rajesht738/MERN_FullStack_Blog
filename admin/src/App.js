639d69f2b7d2fc688cdc7ce95566b7c0f5d75377
Rahul <tiwarirj73@gmail.com>
2022-07-29 01:16:45 +0530

    menu toggle changed

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
-import { AiOutlineMenuFold } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineMenuFold, AiOutlineMenuUnfold} from "react-icons/ai";
 function App() {
+  const [closedNav, setClosedNav] = useState(false);
+  const toggleNav = () => {
+    setClosedNav(!closedNav);
+  }
+  const getNavWidth = () => closedNav ? 'w-16' : 'w-56';
   return (
-   <div className="flex">
-    <div className="w-56 h-screen bg-red-100"></div>
-    <div className="flex-1 min-h-screen bg-blue-100">
-    <button ><AiOutlineMenuFold size={25}/></button>
+    <div className="flex">
+      <div className={getNavWidth() + " h-screen bg-red-100 transition-width"}></div>
+      <div className="flex-1 min-h-screen bg-blue-100">
+        <button onClick={toggleNav}>
+        { closedNav  ? (<AiOutlineMenuUnfold size={25}/>) : (
+          <AiOutlineMenuFold size={25} />
+        ) } 
+        </button>
+      </div>
+
     </div>
-  
-   </div>
   )
 }
 
